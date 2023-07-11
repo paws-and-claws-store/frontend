c937642cffdae7416128cc85af46d3f26d2c2cb5
Dmytro Karpenko <kardmitriy@gmail.com>
2023-07-12 00:09:49 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import { Container, Section } from 'components';
 import { Hero, Title } from './Home.styled';
-import { CarouselBanner } from 'components/Carousel/Carousel';
 import { CardList } from 'components/CardList/CardList';
 
 export const Home = () => {
   return (
     <Section>
       <Container>
-        <Hero>
-          <CarouselBanner />
-        </Hero>
+        <Hero></Hero>
         <Title>акційні пропозиції</Title>
         <CardList />
       </Container>
