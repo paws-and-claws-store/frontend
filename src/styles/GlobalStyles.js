8549009d3a22fd69a3b43b4df7778abbff272840
Dmytro Karpenko <kardmitriy@gmail.com>
2023-07-11 22:47:37 +0300

    Fonts

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import { theme } from 'styles/theme';
 // import '@fontsource-variable/raleway';
-import '@fontsource/raleway/300.css';
 import '@fontsource/raleway/400.css';
 import '@fontsource/raleway/500.css';
 import '@fontsource/raleway/600.css';
@@ -14,7 +13,7 @@ export const GlobalStyles = css`
 
   body {
     /* font-family: 'Raleway Variable', sans-serif; */
-    font-family: 'Raleway';
+    /* font-family: 'Raleway'; */
     margin: 0;
     background: ${theme.colors.mainBackground};
     min-height: 100vh;
