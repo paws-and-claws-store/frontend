1f8504166ea07fde60c6ff406011257a79f6cf6c
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-09 11:48:33 +0300

    categories list

--- src/components/CatalogLayout/CatalogLayout.jsx
+++ src/components/CatalogLayout/CatalogLayout.jsx
@@ -16,7 +16,7 @@ import { Outlet } from 'react-router-dom';
 import { fetchAllPets, fetchAllStructure, fetchProducts } from 'services/api';
 
 export const CatalogLayout = () => {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState('for_dogs');
   const [structure, setStructure] = useState([]);
   const [productsList, setProductsList] = useState([]);
   const [petCollection, setPetCollection] = useState([]);
@@ -38,14 +38,14 @@ export const CatalogLayout = () => {
 
   useEffect(() => {
     fetchAllStructure().then(res => setStructure([...res]));
-    fetchAllPets().then(res => setPetCollection([...res]));
+    // fetchAllPets().then(res => setPetCollection([...res]));
     //fetchAllProsucts()
     fetchProducts().then(res => setProductsList([...res]));
   }, []);
 
-  useEffect(() => {
-    console.log('structure:', structure);
-  }, [structure]);
+  // useEffect(() => {
+  //   console.log('structure:', structure);
+  // }, [structure]);
 
   return (
     <>
@@ -59,13 +59,16 @@ export const CatalogLayout = () => {
                 style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}
               >
                 {structure.map((el, i) => {
+                  // console.log('el:', el);
                   switch (el.code) {
                     case 'for_dogs':
                       return (
                         <li key={i}>
                           <PetButton
-                            id={'dog'}
-                            className={active === 'dog' ? 'active' : undefined}
+                            id={el.code}
+                            className={
+                              active === 'for_dogs' ? 'active' : undefined
+                            }
                             onClick={handleClick}
                             onBlur={() => {
                               setActive('');
@@ -88,8 +91,10 @@ export const CatalogLayout = () => {
                       return (
                         <li key={i}>
                           <PetButton
-                            id={'cat'}
-                            className={active === 'cat' ? 'active' : undefined}
+                            id={el.code}
+                            className={
+                              active === 'for_cats' ? 'active' : undefined
+                            }
                             onClick={handleClick}
                             onBlur={() => {
                               setActive('');
@@ -114,49 +119,43 @@ export const CatalogLayout = () => {
                   }
                 })}
               </ul>
-
-              {/* <PetButton
-              id={'dog'}
-              className={active === 'dog' ? 'active' : undefined}
-              onClick={handleClick}
-              onBlur={() => {
-                setActive('');
-                document.getElementById('hidden').style.visibility = 'hidden';
-              }}
-            >
-              <span>
-                <Dog />
-                <span>Собаки</span>
-              </span>
-              <span>
-                <RightArrow />
-              </span>
-            </PetButton> */}
-
-              {/* <PetButton
-              id={'cat'}
-              className={active === 'cat' ? 'active' : undefined}
-              onClick={handleClick}
-              onBlur={() => {
-                setActive('');
-                document.getElementById('hidden').style.visibility = 'hidden';
-              }}
-            >
-              <span>
-                <Cat />
-                <span>Коти</span>
-              </span>
-              <span>
-                <RightArrow />
-              </span>
-            </PetButton> */}
             </CategoryList>
           )}
         </AsideCatalog>
         <WrapperCatalog>
-          {/* {(active === 'cat' && (
-            <BoxHiden className={active ? 'active' : undefined}>
-              <ul>
+          <BoxHiden className={active ? 'active' : undefined}>
+            {structure
+              .filter(it => it.code === active)
+              .map(({ _categories, _id }) => {
+                console.log(' _categories:', _categories);
+                return (
+                  <ul key={_id}>
+                    {_categories.map(({ code, ua, _id, _variants }) => {
+                      return (
+                        <>
+                          <p>{ua}</p>
+                          <li key={_id}>
+                            <ul>
+                              {_variants.map(({ _id, ua }) => {
+                                return <li key={_id}>{ua}</li>;
+                              })}
+                            </ul>
+                          </li>
+                        </>
+                      );
+                    })}
+                  </ul>
+                );
+
+                // console.log(_categories);
+                // return (
+                //   <li key={_id}>
+                //     <span>{_categories.ua}</span>
+                //   </li>
+                // );
+              })}
+
+            {/* <ul>
                 {uniqueObjArray(catCatalog, 'category').map((el, i) => {
                   console.log('Cat category', el);
                   return (
@@ -208,9 +207,9 @@ export const CatalogLayout = () => {
                       </>
                     );
                   })}
-                </ul>
-              </BoxHiden>
-            ))} */}
+                </ul> */}
+          </BoxHiden>
+
           <Outlet />
         </WrapperCatalog>
       </CatalogContainer>
