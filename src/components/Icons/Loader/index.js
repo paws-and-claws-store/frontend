9fda11792aeb21ca03768846e7fb33fb80e5db8e
Dmytro Karpenko <kardmitriy@gmail.com>
2023-10-13 15:50:05 +0300

    loader

---
+++ src/components/Icons/Loader/index.js
@@ -0,0 +1,6 @@
+export * from './Item_1';
+export * from './Item_2';
+export * from './Item_3';
+export * from './Item_4';
+export * from './Item_5';
+export * from './Item_6';
--- src/pages/PricesDrop.jsx
+++ src/pages/PricesDrop.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Box } from './Home.styled';
+import Loader from 'components/Loader/Loader';
 
 export const PricesDrop = () => {
   return (
     <Box>
       <h1>Акції</h1>
+      <Loader />
     </Box>
   );
 };
