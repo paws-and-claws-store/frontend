a64e036d8ec274c338c745b1188459298106f191
Dmytro Karpenko <kardmitriy@gmail.com>
2023-10-27 13:09:49 +0300

    finished cart without stylization

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,16 +2,38 @@ import React from 'react';
 import {
   BtnBackToCatalog,
   CartContainer,
+  LinkToCatalog,
   ListItems,
   TitleCart,
+  TotalAmount,
+  TotalAmountContainer,
+  TotalAmountNumber,
+  TotalAmountSumbol,
 } from './Cart.styled';
 import Img from '../images/Travel_bag.png';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectCartStore } from 'redux/selectors';
 import { CartItem } from 'components';
 
+import { CaretLeftPagination, CrossToDelete } from 'components/Icons';
+import { clearCartItems } from 'redux/cartSlice';
+
 export const Cart = () => {
   const cartStore = useSelector(selectCartStore);
+  const dispatch = useDispatch();
+
+  const calculateTotalCost = () => {
+    return cartStore.reduce((total, item) => {
+      const itemCost = item.sale
+        ? item.sale * item.cardCount
+        : item.price * item.cardCount;
+      return total + itemCost;
+    }, 0);
+  };
+
+  const handleDelete = () => {
+    dispatch(clearCartItems());
+  };
 
   return (
     <>
@@ -27,6 +49,26 @@ export const Cart = () => {
               );
             })}
           </ListItems>
+          <TotalAmountContainer>
+            <TotalAmount>
+              Загальна сума:{' '}
+              <span>
+                <TotalAmountNumber>
+                  {calculateTotalCost().toFixed(2)}
+                </TotalAmountNumber>
+                <TotalAmountSumbol>₴</TotalAmountSumbol>
+              </span>
+              <button onClick={handleDelete}>
+                <CrossToDelete />
+              </button>
+            </TotalAmount>
+            <LinkToCatalog to={'/catalog'}>
+              <span>
+                <CaretLeftPagination />
+              </span>{' '}
+              <span>Повернутись до каталогу</span>
+            </LinkToCatalog>
+          </TotalAmountContainer>
         </div>
       ) : (
         <CartContainer>
--- src/pages/Cart.styled.jsx
+++ src/pages/Cart.styled.jsx
@@ -53,3 +53,74 @@ export const ListItems = styled.ul`
   gap: 24px;
   margin-top: 40px;
 `;
+
+export const TotalAmountContainer = styled.div`
+  width: 522px;
+  border-top: 1px solid ${theme.colors.secGreen};
+  margin-left: auto;
+  margin-bottom: 355px;
+`;
+
+export const TotalAmount = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 35px;
+
+  margin-top: 41px;
+  color: ${theme.colors.green};
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 32px;
+
+  & span {
+    color: ${theme.colors.orange};
+  }
+`;
+
+export const TotalAmountNumber = styled.span`
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+export const TotalAmountSumbol = styled.span`
+  margin-left: 4px;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 40px; /* 142.857% */
+  text-transform: uppercase;
+`;
+
+export const LinkToCatalog = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 8px;
+  width: 148px;
+  height: 40px;
+
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+
+  color: ${theme.colors.orange};
+  & svg {
+    fill: ${theme.colors.orange};
+  }
+
+  &:hover,
+  &:focus,
+  &:hover svg,
+  &:focus svg {
+    color: ${theme.colors.green};
+    fill: ${theme.colors.green};
+  }
+  &:active,
+  &:active svg {
+    color: ${theme.colors.secGreen};
+    fill: ${theme.colors.secGreen};
+  }
+`;
