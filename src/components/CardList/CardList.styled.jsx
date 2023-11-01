c57f329ffe24d3baaa6d3b40f1a048dd41333607
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-11-01 14:35:34 +0200

    resolve conflicts

--- src/components/CardList/CardList.styled.jsx
+++ src/components/CardList/CardList.styled.jsx
@@ -1,20 +1,14 @@
 import styled from '@emotion/styled';
 
 export const List = styled.ul`
-  display: flex;
+  /* display: flex;
   flex-wrap: wrap;
-<<<<<<< HEAD
-  gap: ${props => props.theme.spacing.step * 5}px; */
+  gap: ${props => props.theme.spacing.step * 5}px;  */
   display: grid;
   /* grid-template-columns: 1fr 1fr 1fr 1fr; */
   grid-template-columns: ${props => props.gridColumns};
-=======
-  gap: ${props => props.theme.spacing.step * 5}px;
-  /* display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
->>>>>>> dev
   grid-column-gap: 20px;
-  grid-row-gap: 20px; */
+  grid-row-gap: 20px;
 `;
 
 export const ListItem = styled.li``;
