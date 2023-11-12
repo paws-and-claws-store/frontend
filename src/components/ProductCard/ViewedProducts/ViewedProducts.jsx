b4d50494a40db4e674d8261ac2d90d6542d24869
Dmytro Karpenko <kardmitriy@gmail.com>
2023-11-12 21:43:44 +0200

    update

--- src/components/ProductCard/ViewedProducts/ViewedProducts.jsx
+++ src/components/ProductCard/ViewedProducts/ViewedProducts.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from //  { useEffect, useState }
+'react';
 import { useSelector } from 'react-redux';
-import { ViewedProductsContainer } from './ViewedProducts.styled'; 
+import { ViewedProductsContainer } from './ViewedProducts.styled';
 import { selectViewedProducts } from 'redux/selectors';
 import { Card } from 'components/Card/Card';
 
 export const ViewedProducts = () => {
+  const viewedProducts = useSelector(selectViewedProducts);
+  console.log('viewedProducts:', viewedProducts);
 
-    const viewedProducts = useSelector(selectViewedProducts);
-    console.log("viewedProducts:", viewedProducts)
-    
-return (
+  return (
     <ViewedProductsContainer>
-        <h3>Переглянуті товари</h3>
-        <ul style={{display: 'flex', gap: '20px'}}>
-            {viewedProducts.length > 0 ? viewedProducts.map((el, indx)=>{return <li key={indx}>
-            <Card el={el} />
-            </li>}).slice(0,4)
-            : null}
-        </ul>
-    
+      <h3>Переглянуті товари</h3>
+      <ul style={{ display: 'flex', gap: '20px' }}>
+        {viewedProducts.length > 0
+          ? viewedProducts
+              .map((el, indx) => {
+                return (
+                  <li key={indx}>
+                    <Card el={el} />
+                  </li>
+                );
+              })
+              .slice(0, 4)
+          : null}
+      </ul>
     </ViewedProductsContainer>
-)
+  );
 };
