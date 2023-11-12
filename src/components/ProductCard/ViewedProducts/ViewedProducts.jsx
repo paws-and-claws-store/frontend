aa36b6ed5ded21afef44824871e3ac2d31d72f1e
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-11-12 20:06:10 +0100

    fixed viewed products

--- src/components/ProductCard/ViewedProducts/ViewedProducts.jsx
+++ src/components/ProductCard/ViewedProducts/ViewedProducts.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { ViewedProductsContainer } from './ViewedProducts.styled'; 
+import { ViewedProductsContainer, ViewedProductsTitel } from './ViewedProducts.styled'; 
 import { selectViewedProducts } from 'redux/selectors';
 import { Card } from 'components/Card/Card';
 
 export const ViewedProducts = () => {
 
     const viewedProducts = useSelector(selectViewedProducts);
-    console.log("viewedProducts:", viewedProducts)
+    // console.log("viewedProducts:", viewedProducts)
     
 return (
     <ViewedProductsContainer>
-        <h3>Переглянуті товари</h3>
+        <ViewedProductsTitel style={{}}>Переглянуті товари</ViewedProductsTitel>
         <ul style={{display: 'flex', gap: '20px'}}>
             {viewedProducts.length > 0 ? viewedProducts.map((el, indx)=>{return <li key={indx}>
             <Card el={el} />
