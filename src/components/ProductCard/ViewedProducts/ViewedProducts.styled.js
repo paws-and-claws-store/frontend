d0dd7329085588f05dd51508c5377d402201038e
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-11-15 22:09:55 +0100

    fixed styles viewedProduct

--- src/components/ProductCard/ViewedProducts/ViewedProducts.styled.js
+++ src/components/ProductCard/ViewedProducts/ViewedProducts.styled.js
@@ -4,7 +4,7 @@ export const ViewedProductsContainer = styled.div`
   width: 100%;
 `;
 
-export const ViewedProductsTitel = styled.h3`
+export const ViewedProductsTitel = styled.p`
   margin-top: 48px;
   margin-bottom: 28px;
   font-size: 24px;
