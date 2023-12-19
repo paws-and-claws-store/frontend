9fc306214a61f06766cedfc1c5128c8e8d84e984
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-12-19 16:53:32 +0200

    refactoring price range slider behaviour, based on task OS-234  requirements

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,8 +4,6 @@ export const selectSearchQueryStore = state => state.search.query;
 export const selectSortingTypeStore = state => state.sorting.type; // select current type of sorting
 export const selectSortingTypeStoreDefault = state => state.sorting.defaultType; // select default type of sorting
 export const selectViewedProducts = state => state.viewedProducts.viewedList;
-export const selectMinPriceRange = state => state.priceRange.minPriceRange;
-export const selectMaxPriceRange = state => state.priceRange.maxPriceRange;
 export const selectPriceValue = state => state.priceRange.value;
 export const selectIsPriceRangeSet = state => state.priceRange.isPriceRangeSet; // select to get setted or not price range filter now
 export const selectIsBrandsFilterSet = state => state.brandsFilter.isBrandsSet; // select to get setted or not brands filter now
@@ -13,3 +11,4 @@ export const selectPriceValueInput = state => state.priceRange.valueInput;
 export const selectIsClearSetPriceRange = state => state.priceRange.isClearSet; // status of clear button set on Price Range filter
 export const selectIsClearSetBrandsFilter = state => state.brandsFilter.isClearSet; // status of clear button set on Brands filter
 export const selectBrandsFilter = state => state.brandsFilter.brands; // select list of selectrd brands in Brands filter
+export const selectDefaultPriceRange = state => state.priceRange.defaultPriceRange; // select default price range for query
