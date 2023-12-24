8fe5d3890b42da5e9cd514015242e01fcdbb1cf2
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-12-24 14:59:30 +0100

    fixed routes

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -3,8 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const { isLoggedIn } = useAuth();
-
-  return isLoggedIn ? children : <Navigate to="/" />;
+  return isLoggedIn ? children : <Navigate to={'/user'} />;
 };
 
 export default PrivateRoute;
