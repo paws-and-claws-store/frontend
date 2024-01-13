2f5b506826afce1dcac777eb3d05342d9775bc1f
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2024-01-13 20:21:36 +0100

    helper and validation text

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from 'hooks/useAuth';
+import React from 'react';
 import { RegistrationContainer } from './Registration.styled';
 import { AuthForm } from 'components/AuthForm/AuthForm';
 
 export const Registration = () => {
-
-  const navigate = useNavigate();
-  const { isLoggedIn } = useAuth();
-
-  useEffect(() => {
-    const handleRegistration = () =>{
-      if (isLoggedIn) {
-        navigate('/');
-      }
-    }
-
-   window.addEventListener('focus', handleRegistration);
-
-   return ()=>{
-    window.removeEventListener('focus', handleRegistration);
-   }
-  },);
 
   return (
     <RegistrationContainer>
